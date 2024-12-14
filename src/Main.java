3c883dd00a0b1679d8c4c70d2015e1e17e717790
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2024-12-14 17:17:30 +0600

    Checking for Readability of movies.using json FileUtil.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import models.Movie;
+import utils.FileUtil;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Movie[] movies = FileUtil.readMovieFile();
+        for (Movie movie : movies) {
+            System.out.println("Name: " + movie.getName());
+            System.out.println("Year: " + movie.getYear());
+            System.out.println("Description: " + movie.getDescription());
+            System.out.println("Director: " + movie.getDirector().getFullName());
+            System.out.println("Cast:");
+            movie.getCast().forEach(castMember ->
+                    System.out.println("  - Actor: " + castMember.getFullName() + ", Role: " + castMember.getRole())
+            );
+            System.out.println();
+        }
     }
 }
