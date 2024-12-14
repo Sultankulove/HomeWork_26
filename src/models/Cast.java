8e9d5d4a3cafefad775c242130e15f36c30b0d96
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2024-12-14 17:14:03 +0600

    Created Cast.java

---
+++ src/models/Cast.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Cast {
+    String fullName;
+    String role;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
