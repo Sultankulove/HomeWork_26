a46fbabd335911bc5ba0d8433fde311210a5f28f
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2024-12-17 14:05:17 +0600

    output

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,47 @@
 import models.Movie;
+import models.OutputOfTheEntireJSON;
+import models.SortMovies;
 import utils.FileUtil;
+import utils.Search;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Movie[] movies = FileUtil.readMovieFile();
-        for (Movie movie : movies) {
-            System.out.println("Name: " + movie.getName());
-            System.out.println("Year: " + movie.getYear());
-            System.out.println("Description: " + movie.getDescription());
-            System.out.println("Director: " + movie.getDirector().getFullName());
-            System.out.println("Cast:");
-            movie.getCast().forEach(castMember ->
-                    System.out.println("  - Actor: " + castMember.getFullName() + ", Role: " + castMember.getRole())
-            );
-            System.out.println();
-        }
+
+        List<Movie> movies = FileUtil.readMovieFileToList();
+
+//        Output output = new Output();
+//        output.OutputMoviesName();
+//        System.out.println("Искать фильмы: ");
+//        System.out.println("a - по полному совпадению");
+//        System.out.println("b - по частичному совпадению");
+//
+//        Scanner sc = new Scanner(System.in);
+//        String searchMovie = sc.nextLine();
+//        Search search = new Search();
+//        String searchNameInput;
+//        switch (searchMovie.toLowerCase()) {
+//            case "a": {
+//                System.out.println("Введите полное название фильмы: ");
+//                searchNameInput = sc.nextLine().toLowerCase();
+//                search.searchByExactName(movies,searchNameInput);
+//                break;
+//            }
+//            case "b": {
+//                System.out.println("Введите частичное название фильмы: ");
+//                searchNameInput = sc.nextLine().toLowerCase();
+//                search.searchByPartialName(movies, searchNameInput);
+//                break;
+//            }
+//            default: {
+//                System.out.println("Ведите заново");
+//
+//            }
+//        }
+
+        SortMovies sm = new SortMovies();
+        sm.getSortMovies();
     }
 }
