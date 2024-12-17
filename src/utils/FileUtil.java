5f6a92004d62fe3e0a5caa5d88ac7f8d33fa849b
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2024-12-17 14:06:02 +0600

    Created for List<Movie>

--- src/utils/FileUtil.java
+++ src/utils/FileUtil.java
@@ -8,26 +8,23 @@ import models.MovieContainer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 
 public class FileUtil {
 
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
-    public static Movie[] readMovieFile() {
-
+    public static List<Movie> readMovieFileToList() {
         try {
             String str = Files.readString(Paths.get("src/data/movies.json"));
-
             MovieContainer container = GSON.fromJson(str, MovieContainer.class);
 
-            return container.getMovies().toArray(new Movie[0]);
+            return container.getMovies();
         } catch (IOException e) {
-
             System.err.println("Error reading the file: " + e.getMessage());
             e.printStackTrace();
-
         }
-
-        return new Movie[0];
+        return Collections.emptyList();
     }
 }
